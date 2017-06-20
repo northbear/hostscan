bf4ab7d7f940b05479d7dab7cc5dc9a8978e652d
Igor Yurchenko <igoryu@mellanox.com>
2017-06-20 17:29:35 +0300

    modify convert tool for using stdin/stdout

--- convert.py
+++ convert.py
@@ -4,25 +4,14 @@
 import json
 import sys
 
-class DbTotals:
-    def __init__(self, db):
-        self.__db = db
-        self.__totals = { 'total': len(db) }
-
-    def calc(self, value, cond): 
-        pass
-        
-
-    def string(self):
-       return str(self.__totals)
-
+from hostdb import HostDB
 
 def load(fp):
     jsn = {}
     if isinstance(fp, basestring):
         with open(fp) as f:
             jsn = json.load(f)
-    elif isinstance(fp, File):
+    elif isinstance(fp, file):
         jsn = json.load(fp)
     else:
         raise TypeError("parameter should be file or string containing path to file")
@@ -32,20 +21,15 @@ def conv2csv(db, fields, outp):
     pass
 
 def rec2str(rec):
-    return ', '.join([rec.get('owner', ''), rec.get('fqdn', ''), rec.get('ip', ''), rec.get('cpu', ''), rec.get('ram', '')])
+    return ', '.join([rec.get('owner', ''), rec.get('fqdn', ''), rec.get('ip', ''), rec.get('cpu', ''), rec.get('ram', ''), rec.get('hcas', '')])
 
 def main():
     config = { 'input': sys.stdin, 'output': sys.stdin }
     
-    config['input'] = "results/hostinfo170618.json"
     db = load(config['input'])
     for k in db:
         rec = db[k]
         print ', '.join([k, rec2str(rec)]) 
-    stat = DbTotals(db) 
-    print "stat:", stat.string()
-    flist = []
-    conv2csv(db, flist, config['output'])
 
 
 if __name__ == '__main__':
